6e6800ece5235ccdf5c2776aa01fec48dc040202
feiteng <2210711471@qq.com>
2017-06-22 14:19:12 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,9 +4,15 @@
 var oBut = document.getElementsByClassName("but");
 var oFont = document.getElementById("show");
 var oProg = document.getElementsByClassName("prog");
+
+var oP = document.getElementsByClassName("text");
+
 var oButLen = oBut.length;
 var width = [0,0,0,0];
 var rgb = [0,0,0];
+
+var text = oP.innerText = "1";
+console.log(text);
 for (var i=0;i<oButLen;i++) {
     oBut[i].index = i;//定义一个index属性  值是i
     oBut[i].onmousedown = function (e) {//鼠标按下那一刻
@@ -16,18 +22,20 @@ for (var i=0;i<oButLen;i++) {
         var lastLeft = width[this.index];
         document.onmousemove = function (e) {
             width[thisIndex.index] = e.clientX - X + lastLeft;//移动改变的值减掉按下去的值
-            if(width[thisIndex.index]>133)width[thisIndex.index]=133;
+            if(width[thisIndex.index]>146)width[thisIndex.index]=146;
             if(width[thisIndex.index]<0)width[thisIndex.index]=0;
 
             oBut[thisIndex.index].style.left = width[thisIndex.index] + "px";
             oProg[thisIndex.index].style.width = width[thisIndex.index] + "px";
+
+
             if (thisIndex.index == 0){
-                oFont.style.fontSize = width[thisIndex.index]/150*40+8+"px"
+                oFont.style.fontSize = width[thisIndex.index]/146*40+8+"px"
             }else {
-                var num = parseInt(width[thisIndex.index]/150*255);
+                var num = parseInt(width[thisIndex.index]/146*255);
                 rgb[thisIndex.index-1] = num;
                 oFont.style.color = "rgb("+rgb[0]+","+rgb[1]+","+rgb[2]+")";
-            };
+            }
         };
         document.onmouseup = function () {
             this.onmousemove = null;
